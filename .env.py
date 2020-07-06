6029eb1ff8bd9f710903e756227e7135ce89dda0
Fatima Aminu <teemamin@yahoo.com>
2020-07-06 13:24:47 +0000

    .env.py deleted

--- .env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_URI"] = "mongodb+srv://root:<mygreengreen111>@myfirstcluster-qycng.mongodb.net/<online_cookbook>?retryWrites=true&w=majority"
-os.environ["MONGODB_NAME"] = "online_cookboook"
