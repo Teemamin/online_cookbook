528773e6ae968de56752dbf429d24231488f5f90
Fatima Aminu <teemamin@yahoo.com>
2020-07-07 14:28:21 +0000

    added bootstrap theme and dependencies,base-template for the recipe site,read functionality in flask

--- app.py
+++ app.py
@@ -12,10 +12,25 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGODB_NAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
-
-@app.route("/")
+@app.route('/')
+@app.route("/home")
 def home():
-    return "<h1>Hello world</h1>"
+    return render_template("index.html",recipes=mongo.db.recipe_collections.find(),owner=mongo.db.recipe_owner.find())
+
+
+@app.route("/addrecipe")
+def addrecipe():
+    return "<h1>hello again</h1>"
+
+
+@app.route("/get_recipe/<recipe_id>")
+def get_recipe(recipe_id):
+    the_recipe = mongo.db.recipe_collections.find_one({"_id": ObjectId(recipe_id)})
+    the_recipe_owner = mongo.db.recipe_owner.find()
+    return render_template("get_recipe.html", recipes= the_recipe, recipe_owner = the_recipe_owner)
+
+
+
 
 
 if __name__ == '__main__':
